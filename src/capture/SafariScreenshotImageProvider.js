209641686dba01e8981fa04ebe3b8a7cc9576b36
Yurii <yurii.besedin@applitools.com>
2018-01-04 18:28:09 +0200

    WIP: Check region and full page safari support

--- src/capture/SafariScreenshotImageProvider.js
+++ src/capture/SafariScreenshotImageProvider.js
@@ -7,62 +7,57 @@ const WDIOJSExecutor = require('../WDIOJSExecutor');
 
 class SafariScreenshotImageProvider extends ImageProvider {
 
-    /**
-     * @param {Eyes} eyes
-     * @param {Logger} logger A Logger instance.
-     * @param {EyesWebDriver} driver
-     * @param {PromiseFactory} promiseFactory
-     */
-    constructor(eyes, logger, driver, promiseFactory) {
-        super();
+  /**
+   * @param {Eyes} eyes
+   * @param {Logger} logger A Logger instance.
+   * @param {EyesWebDriver} driver
+   * @param {PromiseFactory} promiseFactory
+   */
+  constructor(eyes, logger, driver, promiseFactory) {
+    super();
 
-        this._executor = eyes;
-        this._logger = logger;
-        this._executor = driver;
-        this._promiseFactory = promiseFactory;
+    this._eyes = eyes;
+    this._logger = logger;
+    this._executor = driver;
+    this._promiseFactory = promiseFactory;
 
-        this._jsExecutor = new WDIOJSExecutor(driver);
+    this._jsExecutor = new WDIOJSExecutor(driver);
+  }
+
+  /**
+   * @override
+   * @return {Promise.<MutableImage>}
+   */
+  async getImage() {
+    const that = this;
+    this._logger.verbose("Getting screenshot as base64...");
+    const screenshot64 = await this._executor.takeScreenshot();
+    that._logger.verbose("Done getting base64! Creating MutableImage...");
+    const image = new MutableImage(screenshot64, that._promiseFactory);
+
+    await that._eyes.getDebugScreenshotsProvider().save(image, "SAFARI");
+    if (!that._eyes.getForceFullPageScreenshot()) {
+      const currentFrameChain = that._executor.frameChain;
+
+      let loc;
+      if (currentFrameChain.size() === 0) {
+        const positionProvider = new ScrollPositionProvider(that._logger, that._jsExecutor);
+        loc = await positionProvider.getCurrentPosition();
+      } else {
+        loc = that._promiseFactory.resolve(currentFrameChain.getDefaultContentScrollPosition());
+      }
+
+      that._logger.verbose("frame.getLocation(): " + loc);
+      let viewportSize = await that._eyes.getViewportSize();
+      const scaleRatio = that._eyes.getDevicePixelRatio();
+      viewportSize = viewportSize.scale(scaleRatio);
+      loc = loc.scale(scaleRatio);
+
+      return image.crop(new Region(loc, viewportSize));
     }
 
-    /**
-     * @override
-     * @return {Promise.<MutableImage>}
-     */
-    getImage() {
-        const that = this;
-        this._logger.verbose("Getting screenshot as base64...");
-        return this._executor.takeScreenshot().then(screenshot64 => {
-            that._logger.verbose("Done getting base64! Creating MutableImage...");
-            const image = new MutableImage(screenshot64, that._promiseFactory);
-
-            return that._executor.getDebugScreenshotsProvider().save(image, "SAFARI").then(() => {
-                if (!that._executor.getForceFullPageScreenshot()) {
-                    const currentFrameChain = that._executor.getDriver().getFrameChain();
-
-                    let promise;
-                    if (currentFrameChain.size() === 0) {
-                        const positionProvider = new ScrollPositionProvider(that._logger, that._jsExecutor);
-                        promise = positionProvider.getCurrentPosition();
-                    } else {
-                        promise = that._promiseFactory.resolve(currentFrameChain.getDefaultContentScrollPosition());
-                    }
-
-                    return promise.then(loc => {
-                        that._logger.verbose("frame.getLocation(): " + loc);
-                        return that._executor.getViewportSize().then(viewportSize => {
-                            const scaleRatio = that._executor.getDevicePixelRatio();
-                            viewportSize = viewportSize.scale(scaleRatio);
-                            loc = loc.scale(scaleRatio);
-
-                            return image.crop(new Region(loc, viewportSize));
-                        });
-                    });
-                }
-
-                return image;
-            });
-        });
-    }
+    return image;
+  }
 }
 
 module.exports = SafariScreenshotImageProvider;
