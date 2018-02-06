08b864840ecf579f177aa89d1a6fdd7aec266de9
Yurii <yurii.besedin@applitools.com>
2018-02-06 13:48:57 +0200

    TestSpecialCases - Chrome - ForceFullPageScreenShot

---
+++ test/TestSpecialCases_Chrome_ForceFullPageScreenShot.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {TestSpecialCases} = require('./TestSpecialCases');
+const Common = require('./lib/Common');
+
+
+const appName = 'Eyes Selenium SDK - Special Cases - ForceFPS';
+const testedPageUrl = 'http://applitools.github.io/demo/TestPages/WixLikeTestPage/index.html';
+
+
+const test = new Common({testedPageUrl});
+
+
+describe(appName, function () {
+
+  before(function () {
+    test.beforeTest({appName: appName, fps: true});
+  });
+
+  beforeEach(async function () {
+    await test.beforeEachTest({appName: appName, testName: this.currentTest.title, browserOptions: Common.CHROME});
+  });
+
+  afterEach(async function () {
+    await test.afterEachTest();
+  });
+
+
+  TestSpecialCases.shouldBehaveLike('TestSpecialCases', test);
+
+});
