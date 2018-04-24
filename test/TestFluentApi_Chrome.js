6a5c8fb84f1492562f68129e2773534bc89b82d0
Yurii <yurii.besedin@applitools.com>
2018-04-24 18:13:12 +0300

    Enable fluent api tests

--- test/TestFluentApi_Chrome.js
+++ test/TestFluentApi_Chrome.js
@@ -11,7 +11,7 @@ const testedPageUrl = 'http://applitools.github.io/demo/TestPages/FramesTestPage
 const test = new Common({testedPageUrl});
 
 
-describe.skip(appName, function () {
+describe(appName, function () {
 
   before(function () {
     chromedriver.start();
